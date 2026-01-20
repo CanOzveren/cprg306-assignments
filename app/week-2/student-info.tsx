28caefb324ee55c21cde31dbb02f33ddd315a174
CanOzveren <ozverencan@gmail.com>
2026-01-20 15:42:56 -0700

    first commit

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Can Ozveren</p>
+      <p>
+        <Link href="https://github.com/CanOzveren/cprg306-assignments">
+          https://github.com/CanOzveren/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
